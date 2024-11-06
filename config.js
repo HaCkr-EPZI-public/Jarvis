b588df02f8766278b76d0ec00d621a0de874b2ec
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-11-06 18:26:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,64 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+
+if (fs.existsSync('config.env')) {
+  require('dotenv').config({
+      path: './config.env'
+  });
+};
+
+global.api = 'https://nervous-rosamond-jarvis-bot-99587a26.koyeb.app/';
+
+const toBool = (x) => (x && (x.toLowerCase() === 'true' || x.toLowerCase() === 'on')) || false;
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
+
+module.exports = {
+  VERSION: require('./package.json').version,
+  BAN_CHATS: process.env.BAN_CHATS || "",
+  PORT: toBool(process.env.PORT) || 8000,
+  PM_BLOCKER: toBool(process.env.PM_BLOCKER) || false,
+  PM_BLOCKER_MSG: process.env.PM_BLOCKER_MSG || "```Pm blocker active on this chat!!```",
+  AUDIO_DATA: process.env.AUDIO_DATA || 'Asᴍᴏᴅᴇᴜs-Eᴘᴢɪ;🇶🇺🇪🇪🇳-🇩;https://i.ibb.co/Pr4BfhZ/1500x1500.png',
+  WARN_COUNT: process.env.WARN_COUNT || '3',
+  ANTILINK_MSG: process.env.ANTILINK_MSG || "```Link Not allowed in this group!!```",
+  ANTIBOT_MSG: process.env.ANTIBOT_MSG || "```Antibot deleted successfully!!```",
+  ANTIWORD_MSG: process.env.ANTIWORD_MSG || "```Antiword deleted successfully!!```",
+  ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender Queen D is alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_Epzi Might be Not available here for now u can use me if u like_\n\n_For Commands: ```.menu```",
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_0e9c_892c_c31c_a461_8315_5dde_400c_7451',
+  LANG: process.env.LANG || 'EN',
+  SETVV: process.env.SETVV || 'DM',
+  ELEVENLABS: process.env.ELEVENLABS,
+  HANDLERS: process.env.HANDLERS || process.env.HANDLER || process.env.PREFIX || '.',
+  ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "false"),
+  READ_MSG: toBool(process.env.READ_MSG || "false"),
+  BRANCH: "main",
+  LINKPREVIEW: toBool(process.env.LINKPREVIEW || "false"),
+  CONTEXTINFO: process.env.CONTEXTINFO || `{"title": "ϙᴜᴇᴇɴ-ᴅ", "body": "Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 👽", "thumbnailUrl": "https://i.ibb.co/Pr4BfhZ/1500x1500.png", "renderLargerThumbnail": false, "mediaType": 1, "mediaUrl": "", "sourceUrl": "https://www.instagram.com/yuren.sasanka/", "showAdAttribution": false}`,
+  KOYEB_API: process.env.KOYEB_API,
+  KOYEB_APP_NAME: process.env.KOYEB_APP_NAME,
+  RENDER_API: process.env.RENDER_API,
+  RENDER_APP_NAME: process.env.RENDER_APP_NAME,
+  BRAINSHOP: process.env.BRAINSHOP || '172372,nbjE0YAlyw3cpoMl',
+  TGTOKEN: "bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4",
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '🇶🇺🇪🇪🇳-🇩;ᴡᴀʙᴏᴛ',
+  CALL_BLOCK: toBool(process.env.CALL_BLOCK) || false,
+  SAVE_STATUS: toBool(process.env.SAVE_STATUS) || false,
+  STATUS_VIEW: process.env.STATUS_VIEW || "false",
+  REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+  WELCOME_MSG: process.env.WELCOME_MSG || "Hi &mention Welcome to &gname",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi $mention It was Nice Seeing you",
+  MEDIA_DATA: process.env.MEDIA_DATA|| 'ʟɪꜱᴛ ᴍᴇɴᴜ;ϙᴜᴇᴇɴ-ᴅ;https://i.ibb.co/Pr4BfhZ/1500x1500.png',
+  MENU_FONT: process.env.MENU_FONT || "0;0",
+  SUDO: process.env.SUDO || '94759554531',
+  STARTING_MSG: toBool(process.env.STARTING_MSG) || true,
+  STATUS_REACTION_EMOJI: process.env.STATUS_REACTION_EMOJI || "🍉,🍓",
+  STATUS_REACTION: toBool(process.env.STATUS_REACTION) || false,
+  GEMINI: process.env.GEMINI || "null",
+  LOG_MSG: toBool(process.env.LOG_MSG) || true,
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
+  BOT_INFO: process.env.BOT_INFO || 'ϙᴜᴇᴇɴ-ᴅ;Asᴍᴏᴅᴇᴜs-Eᴘᴢɪ;https://graph.org/file/1506e5842805b0968c5cf.mp4',
+  WORK_TYPE: process.env.WORK_TYPE || 'public',
+  DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
+};
